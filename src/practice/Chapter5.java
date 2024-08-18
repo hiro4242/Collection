232051e2ea65624ed1376d35c3da6c486da643fc
Hirotaka <36596960+hiro4242@users.noreply.github.com>
2024-08-19 01:58:50 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -1,10 +1,28 @@
 package practice;
 
-public class Chapter5 {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
+public class Chapter5{
+	
+public static void main(String[] args) {
+	
+    List<String> tasks = new ArrayList<String>(Arrays.asList(
+            new Task(LocalDate.of(2021, 10, 21), "牛乳を買う。").toString(),
+            new Task(LocalDate.of(2021, 9, 15), "○○社面談。").toString(),
+            new Task(LocalDate.of(2021, 12, 4), "手帳を買う。").toString(),
+            new Task(LocalDate.of(2021, 8, 10), "散髪に行く。").toString(),
+            new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。").toString()
+    		));
+    
+    Collections.sort(tasks);
+    
+    for(int i = 0; i < tasks.size(); i++) {
+    	System.out.println(tasks.get(i));
+    }
+}
 
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -1,5 +1,19 @@
 package practice;
 
+import java.time.LocalDate;
+
 public class Task {
+    LocalDate date;
+    String task;
+
+    Task(LocalDate date, String task) {
+        this.date = date;
+        this.task = task;
+    }
+
+    @Override
+    public String toString() {
+        return date + ":" + task;
+    }
 
 }
