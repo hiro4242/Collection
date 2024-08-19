52efb778954b4d9126508fb1b5c0be344fa2dd01
Hirotaka <36596960+hiro4242@users.noreply.github.com>
2024-08-19 18:30:24 +0900

    稼働提出3

--- src/practice/Task.java
+++ src/practice/Task.java
@@ -2,7 +2,7 @@ package practice;
 
 import java.time.LocalDate;
 
-public class Task {
+public class Task implements Comparable<Task> {
     LocalDate date;
     String task;
 
@@ -15,5 +15,10 @@ public class Task {
     public String toString() {
         return this.date + ":" + this.task;
     }
+
+	@Override
+	public int compareTo(Task other) {
+		return this.date.compareTo(other.date);
+	}
 
 }
