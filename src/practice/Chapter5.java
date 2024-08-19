dd0f2e0a311197550c8440be6a3790f0630cf4ac
Hirotaka <36596960+hiro4242@users.noreply.github.com>
2024-08-19 18:42:54 +0900

    課題提出4

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -11,11 +11,11 @@ public class Chapter5{
 public static void main(String[] args) {
 	
     List<Task> tasks = new ArrayList<>(Arrays.asList(
-            new Task(LocalDate.of(2021, 10, 21), "牛乳を買う。"),
-            new Task(LocalDate.of(2021, 9, 15), "○○社面談。"),
-            new Task(LocalDate.of(2021, 12, 4), "手帳を買う。"),
-            new Task(LocalDate.of(2021, 8, 10), "散髪に行く。"),
-            new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。")
+            new Task(LocalDate.of(2021, 10, 21), "牛乳を買う"),
+            new Task(LocalDate.of(2021, 9, 15), "○○社面談"),
+            new Task(LocalDate.of(2021, 12, 4), "手帳を買う"),
+            new Task(LocalDate.of(2021, 8, 10), "散髪に行く"),
+            new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く")
     		));
     
     Collections.sort(tasks);
